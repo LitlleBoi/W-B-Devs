db3a2473a665b25b9f34bc26952b67108cea2b18
Bilal-1-2 <bilal127g14@gmail.com>
2025-11-18 09:21:14 +0100

    assets

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $images = glob('assets/**.jpg');
+    foreach ($images as $image) {
+
+        echo "<img src='$image' alt='' style='max-width: 200px; margin: 10px;'><br>";
+        echo "" . dirname($image) . "/" . basename($image) . "<br>";
+        ;
+    }
+    $images = glob('assets/**/*.jpg');
+    foreach ($images as $image) {
+
+        echo "<img src='$image' alt='' style='max-width: 200px; margin: 10px;'><br>";
+        echo "" . dirname($image) . "/" . basename($image) . "<br>";
+        ;
+    }
+    ?>
+
+</body>
+
+</html>
