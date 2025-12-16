ab24b8e59c5d8c19361ddd2113c3957f182ea994
Bilal-1-2 <bilal127g14@gmail.com>
2025-12-16 01:56:08 +0100

    Refactor database connection logic to detect environment for local and production setups

--- assets/includes/connectie.php
+++ assets/includes/connectie.php
@@ -1,12 +1,19 @@
 <?php
-// Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = ""; // Default for XAMPP
-$dbname = "w_b_devs_new";
+// Detect environment
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || preg_match('/^192\.168\.2/', $_SERVER['SERVER_ADDR'])) {
+    // Offline/local omgeving
+    $servername = "localhost";  // of je offline host
+    $username = "root";         // offline user
+    $password = "";             // offline password
+    $dbname = "w_b_devs_new_offline"; // offline database naam
+} else {
+    // Online/production omgeving
 
-
-
+    $servername = "localhost";
+    $username = "u240653_w_b_devs";
+    $password = "fTzkCnbeDBnsj7xwrL5g"; // Default for XAMPP
+    $dbname = "u240653_w_b_devs";
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,5 +22,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
--- assets/includes/select.php
+++ assets/includes/select.php
@@ -2,14 +2,21 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-// Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = ""; // Default for XAMPP
-$dbname = "w_b_devs_new";
 
-
-
+// Detect environment
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || preg_match('/^192\.168\.2/', $_SERVER['SERVER_ADDR'])) {
+    // Offline/local omgeving
+    $servername = "localhost";  // of je offline host
+    $username = "root";         // offline user
+    $password = "";             // offline password
+    $dbname = "w_b_devs_new_offline"; // offline database naam
+} else {
+    // Online/production omgeving
+    $servername = "localhost";
+    $username = "u240653_w_b_devs";
+    $password = "fTzkCnbeDBnsj7xwrL5g"; // Default for XAMPP
+    $dbname = "u240653_w_b_devs";
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
